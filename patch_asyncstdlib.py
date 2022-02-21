07d682110d16d588432cc84550b18155e33cb6e7
Max Fischer <maxfischer2781@gmail.com>
2022-02-21 17:38:30 +0100

    them old times...

--- patch_asyncstdlib.py
+++ patch_asyncstdlib.py
@@ -10,5 +10,5 @@ for module in (asyncstdlib._core,):
     with open(module.__file__, "ab") as module_source:
         _, _, name = module.__name__.partition(".")
         module_source.write(
-            b"from asyncstdlib_cy.%(s) import *\n" % name.encode("ascii")
+            b"from asyncstdlib_cy.%s import *\n" % name.encode("ascii")
         )
